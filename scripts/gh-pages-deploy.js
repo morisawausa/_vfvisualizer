0773f80c99ac8edb58d32e79e85a1dc24dd97c28
Nic Schumann <nic@nicschumann.co>
2020-06-03 09:47:22 -0400

    [deploy] adds deploy script

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,24 @@
+const branchName = require('current-git-branch')
+const version = require('../package.json').version;
+const execa = require('execa');
+const FOLDER_NAME = 'dist';
+const BRANCH = branchName() || 'master'
+
+(async function() {
+  try {
+    await execa('git', ['checkout', '--orphan', 'gh-pages'])
+    console.log('Building Application...')
+    await execa('npm', ['run', 'build'])
+    await execa('git', ['--work-tree', FOLDER_NAME, 'add', '--all'])
+    await execa('git', ['--work-tree', FOLDER_NAME, 'commit', '-m', `deploy ${version}`])
+    console.log('Pushing to gh-pages...')
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
+    await execa('rm', ['-rf', FOLDER_NAME])
+    await execa('git', ['checkout', '-f', BRANCH])
+    await execa('git', ['branch', '-D', 'gh-pages'])
+    console.log('Deployed.')
+  } catch (e) {
+    console.error(e.message);
+    process.exit(1)
+  }
+})();
